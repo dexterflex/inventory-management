c5cac0dd07ccdba7ad03eb4c3adc0edf9b95b2d4
VIVEK KUMAR <vivekkumar@VIVEKs-MacBook-Air.local>
2024-05-20 02:27:59 +0530

    update model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -3,9 +3,19 @@
 class productModel {
     constructor(_id, _name, _desc, _price, _imageUrl) {
         id = _id
-        name = _name
+        title = _name
         desc = _desc
         price = _price
         imageUrl = _imageUrl
+    }
+    static get() {
+        prodObj = {
+            "id": id,
+            "title": title,
+            "desc": desc,
+            "price": price,
+            "imageUrl": imageUrl
+        }
+        return prodObj;
     }
 }
